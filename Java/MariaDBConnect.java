e972c9e54775e473661e341e1e2eaf69adb76937
hwilliams-git <78039951+hwilliams-git@users.noreply.github.com>
2021-02-12 15:40:10 -0500

    Update MariaDBConnect.java

--- Java/MariaDBConnect.java
+++ Java/MariaDBConnect.java
@@ -8,8 +8,45 @@ import java.util.List;
 
 public class MariaDBConn
 {
-	/**
-	 ** Don't forget to import the driver into the 
-	 ** Reference Libraries or none of this will work.
-	 **/
+    /**
+    ** Don't forget to import the driver into the 
+    ** Reference Libraries or none of this will work.
+    **/
+  
+    // JDBC driver name and database URL
+    String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
+    String DB_URL = "jdbc:mariadb://localhost:3306/"; // Need to look into how to set this up for other IPs.
+    
+    Connection conn = null;
+    Statement stmt = null;
+	
+    // Connect to database
+    // This function be called everytime a public function needs to access the 
+    // the database.
+    private Statement ConnectDB(String userName, String passWord, String dbName)
+    {
+        try
+        {
+            // Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            // Open a connection
+            // Need to refresh myself on user privileges again.
+            conn = DriverManager.getConnection(DB_URL+dbName, userName, passWord);
+
+            // Create statement
+            stmt = conn.createStatement();
+
+            // Return statement to use in a query
+            return stmt;
+        }catch(Exception e)
+        {
+            // I think the function calling this one will handle 
+            // catching the error. Otherwise it might be printed
+            // twice. It's your choice to keep or remove it.
+            e.printStackTrace();
+        } 
+
+        return stmt;
+    }
 }
