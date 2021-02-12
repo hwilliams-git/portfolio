c07a34fe2a4dcc8aabe439e23b25596c3f115244
hwilliams-git <78039951+hwilliams-git@users.noreply.github.com>
2021-02-12 15:25:22 -0500

    Create MariaDBConnect.java

---
+++ Java/MariaDBConnect.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MariaDBConn
+{
+	/**
+	 ** Don't forget to import the driver into the 
+	 ** Reference Libraries or none of this will work.
+	 **/
+}
